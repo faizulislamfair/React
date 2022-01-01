4fb68dbf2164e353ef4af9843822b3c54b4687da
faizulislamfair <faizulislamfair@gmail.com>
2022-01-01 23:06:04 +0600

    jsx

--- Anisul Islam Tutorials/my-app/src/index.js
+++ Anisul Islam Tutorials/my-app/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const todoTitle = "Coca Cola";
+const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth()+1;
+const currentYear = date.getFullYear();
 
 ReactDOM.render(
-  <h1>Hello welcome to react</h1>,
-  
+  <div>
+  <h1>Hello welcome to react</h1>
+  <h2>{todoTitle}</h2>
+  <h3>kola</h3>
+  <h1>{dateName + "/" + monthName + "/" + currentYear}</h1>
+  </div>,
   document.getElementById('root')
 );
 

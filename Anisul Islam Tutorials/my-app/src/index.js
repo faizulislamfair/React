4af376be1b6fb02510c4610244b87115a26e6ccb
faizulislamfair <faizulislamfair@gmail.com>
2022-01-03 00:27:13 +0600

    css i jsx

--- Anisul Islam Tutorials/my-app/src/index.js
+++ Anisul Islam Tutorials/my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const todoTitle = "Coca Cola";
 const date = new Date();
@@ -7,9 +8,18 @@ const dateName = date.getDate();
 const monthName = date.getMonth()+1;
 const currentYear = date.getFullYear();
 
+const headingStyle = {  
+  backgroundColor: "purple",
+  color: "#fff", 
+  textAlign : "center",
+  fontSize : "5rem" 
+}
+
 ReactDOM.render(
   <div>
-  <h1>Hello welcome to react</h1>
+  <h1 style={headingStyle}>React to the Rescue</h1>
+  <h1 className='ext'>Fair Fair Fair</h1>
+  <h1 className='similar'>Exams Ongoing</h1>
   <h2>{todoTitle}</h2>
   <h3>kola</h3>
   <h1>{dateName + "/" + monthName + "/" + currentYear}</h1>

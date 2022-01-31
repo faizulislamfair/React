70ff0dc270d18f253c3af1a69211f0c7796a970a
faizulislamfair <faizulislamfair@gmail.com>
2022-01-31 20:49:45 +0600

    form, useState, onChange in functional component

--- Anisul Islam Tutorials/my-app/src/App.js
+++ Anisul Islam Tutorials/my-app/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import HOOKS_USESTATE1 from "./HOOKS_USESTATE/index1";
-import HOOKS_USESTATE2 from "./HOOKS_USESTATE/index2";
+import FORM from "./FORM/FORM";
 
 function App() {
      return(
        <div>
-         <HOOKS_USESTATE1 />
-         <HOOKS_USESTATE2 />
+        <FORM />
        </div>
      )
 }
